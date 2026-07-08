fe78e3ad8926f9fa8e4f3421afb768a40fc9fc79
Devansh Singh <devanshsingh75020@gmail.com>
2026-07-08 21:51:14 +0530

    Time: 314 ms (13.38%) | Memory: 189.7 MB (36.01%) - LeetSync

--- 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.cpp
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.cpp
@@ -29,10 +29,3 @@ public:
         return ans;
     }
 };
-
-/**
- * Your FindSumPairs object will be instantiated and called as such:
- * FindSumPairs* obj = new FindSumPairs(nums1, nums2);
- * obj->add(index,val);
- * int param_2 = obj->count(tot);
- */
