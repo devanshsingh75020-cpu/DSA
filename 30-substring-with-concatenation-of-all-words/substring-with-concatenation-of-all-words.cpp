d2d85553833fa01c4ab9e53934cfb669bc8f72c0
Devansh Singh <devanshsingh75020@gmail.com>
2026-07-26 03:26:36 +0530

    Time: 15 ms (92.86%) | Memory: 23.7 MB (86.36%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
@@ -4,27 +4,56 @@ public:
 
         vector<int> ans;
 
-        int m = words.size();
-        int k = words[0].size();
-        int window = m * k;
+        unordered_map<string, int> need;
+        for (auto &word : words)
+            need[word]++;
 
-        unordered_map<string,int> need;
+        int wordLen = words[0].size();
+        int totalWords = words.size();
+        int n = s.size();
 
-        for(auto &w : words)
-            need[w]++;
+        // Try every possible starting offset
+        for (int start = 0; start < wordLen; start++) {
 
-        for(int i = 0; i + window <= s.size(); i++) {
+            unordered_map<string, int> have;
 
-            unordered_map<string,int> have;
+            int left = start;
+            int count = 0;
 
-            for(int j = 0; j < window; j += k) {
+            for (int right = start; right + wordLen <= n; right += wordLen) {
 
-                string word = s.substr(i + j, k);
+                string word = s.substr(right, wordLen);
+
+                // Invalid word
+                if (need.find(word) == need.end()) {
+                    have.clear();
+                    count = 0;
+                    left = right + wordLen;
+                    continue;
+                }
+
                 have[word]++;
-            }
+                count++;
 
-            if(have == need)
-                ans.push_back(i);
+                // Remove extra copies
+                while (have[word] > need[word]) {
+                    string leftWord = s.substr(left, wordLen);
+                    have[leftWord]--;
+                    left += wordLen;
+                    count--;
+                }
+
+                // Found a valid window
+                if (count == totalWords) {
+                    ans.push_back(left);
+
+                    // Slide window by one word
+                    string leftWord = s.substr(left, wordLen);
+                    have[leftWord]--;
+                    left += wordLen;
+                    count--;
+                }
+            }
         }
 
         return ans;
