573b24f6c55057fdfc68e42bb23a46886dd1d4a1
Devansh Singh <devanshsingh75020@gmail.com>
2026-08-02 22:35:32 +0530

    Time: 3 ms (24.80%) | Memory: 7.8 MB (39.83%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -2,20 +2,32 @@ class Solution {
 public:
     int minFlips(int a, int b, int c) {
         int cnt=0;
-        for(int i=0;i<32;i++){
-        int bitA = (a >> i) & 1;
-        int bitB = (b >> i) & 1;
-        int bitC = (c >> i) & 1;
-        if(bitC==1){
-            if((bitA |bitB)==1) continue;
-            else cnt++;
-        }else{
-            if((bitA | bitB)==0) continue;
-            else{
-                if(bitA==1 && bitB==1) cnt=cnt+2;
-                else cnt++;
+        int orab = a|b;
+
+        for(int i=0; i<32; i++){
+        int bitA = (a & (1<<i))>>i;
+        int bitB = (b & (1<<i))>>i;
+        int bitC = (c & (1<<i))>>i;
+        int x = (orab & (1<<i))>>i;
+
+        if(x != bitC){
+            if(bitA == 0 && bitB==0){
+               if(bitC==1){
+                cnt++;
+               }
+            }
+            else if((bitA == 0 && bitB == 1) || (bitA == 1 && bitB == 0)){
+                if(bitC==0){
+                    cnt++;
+                }
+            }
+            else if(bitA ==1 && bitB==1){
+                if(bitC==0){
+                    cnt+=2;
+                }
             }
         }
+        }
+        return  cnt;
     }
-return  cnt;}
 };
