ca36873b8d12914c33b7dc58e7527f85349730a1
Devansh Singh <devanshsingh75020@gmail.com>
2026-08-07 01:06:40 +0530

    Time: 12 ms (33.88%) | Memory: 21.3 MB (37.16%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,28 +1,48 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        stack<int> st;
-        int n = heights.size();
-        int maxArea = 0;
+    int largestRectangleArea(vector<int>&heights){
+        stack<int>st;
+        int n=heights.size();
+        int maxArea=0;
+        vector<int>nsl(n,-1);
+        vector<int>nsr(n,n);
 
-        for (int i = 0; i <= n; i++) {
-            int currHeight = (i == n) ? 0 : heights[i];
-            while (!st.empty() && currHeight < heights[st.top()]) {
-                int h = heights[st.top()];
+        for(int i=n-1; i>=0; i--){
+            while(!st.empty() && heights[i] <= heights[st.top()]){
                 st.pop();
-                int w = st.empty() ? i : i - st.top() - 1;
-                maxArea = max(maxArea, h * w);
+            }
+
+            if(!st.empty()){
+                nsr[i]=st.top();
             }
             st.push(i);
         }
+
+        while (!st.empty()) st.pop();
+
+        for(int i=0; i<n;  i++){
+            while(!st.empty() && heights[i] <= heights[st.top()]){
+                st.pop();
+            }
+
+            if(!st.empty()){
+                nsl[i]=st.top();
+            }
+            st.push(i);
+        }
+
+        for(int i=0; i<n; i++){
+            maxArea=max(maxArea,heights[i]*(nsr[i]-nsl[i]-1));
+        }
         return maxArea;
     }
-
     int maximalRectangle(vector<vector<char>>& matrix) {
         if(matrix.empty()) return 0;
-        int rows = matrix.size();
-        int cols = matrix[0].size();
+
+        int rows=matrix.size();
+        int cols=matrix[0].size();
         vector<int> heights(cols, 0);
+
         int ans = 0;
          for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
