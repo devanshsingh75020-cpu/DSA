5d460a69051b5530354f760d5e41f82870475713
Devansh Singh <devanshsingh75020@gmail.com>
2026-08-20 03:42:53 +0530

    Time: 48 ms (42.52%) | Memory: 116.9 MB (10.48%) - LeetSync

---
+++ 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> validSequence(string word1, string word2) {
+        int n = word1.length();
+        int m = word2.length();
+        vector<int> rightHandSideMatchLength(n, 0);
+
+        int rightMatched = 0;
+        int i = n - 1;
+        int j = m - 1;
+        while (i >= 0) {
+            if (j >= 0 && word1[i] == word2[j]) {
+                rightMatched++;
+                j--;
+            }
+            rightHandSideMatchLength[i] = rightMatched;
+            i--;
+        }
+
+        vector<int> seq;
+        bool changePower = true;
+        i = 0;
+        j = 0;
+        while (i < n && j < m) {
+            if (word1[i] == word2[j]) {
+                seq.push_back(i);
+                j++;
+            } else if (changePower) {
+                int remNeeded = m - j - 1;
+                int availableSuffixMatch = (i + 1 < n) ? rightHandSideMatchLength[i + 1] : 0;
+                if (availableSuffixMatch >= remNeeded) {
+                    seq.push_back(i);
+                    j++;
+                    changePower = false;
+                }
+            }
+            i++;
+        }
+        return (seq.size() == m) ? seq : vector<int>{};
+    }
+};
