e9f2950e21de26854bc66c27c28e0322ef5220ed
Devansh Singh <devanshsingh75020@gmail.com>
2026-08-22 22:48:55 +0530

    Time: 158 ms (19.28%) | Memory: 146 MB (11.65%) - LeetSync

--- 2388-replace-elements-in-an-array/replace-elements-in-an-array.cpp
+++ 2388-replace-elements-in-an-array/replace-elements-in-an-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
-        map<int, int> mp;
+        unordered_map<int, int> mp;
         for(int i = 0; i < nums.size(); i++) {
             mp[nums[i]] = i;
         }
@@ -11,9 +11,7 @@ public:
             int newValue = op[1];
 
             int idx = mp[oldValue];
-
             nums[idx] = newValue;
-
             mp.erase(oldValue);
             mp[newValue] = idx;
         }
