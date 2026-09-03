3bfc230f26ee99f2a4ea0a870ac00f2f2b3f5a78
Devansh Singh <devanshsingh75020@gmail.com>
2026-09-03 20:31:23 +0530

    Time: 61 ms (25.92%) | Memory: 166.9 MB (17.02%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
         int n = nums1.size();
-
         sort(nums1.begin(), nums1.end());
 
-        // Case 1: all elements are even
         bool allEven = true;
 
         for(int i = 0; i < n; i++) {
@@ -14,13 +12,8 @@ public:
                 break;
             }
         }
+        if(allEven) return true;
 
-        if(allEven)
-            return true;
-
-        // Prefix array:
-        // smalloddpr[i] = is there an odd element
-        // from index 0 to i?
         vector<bool> smalloddpr(n, false);
 
         if(nums1[0] % 2 == 1)
@@ -32,17 +25,13 @@ public:
                 smalloddpr[i] = true;
         }
 
-        // For every even element, there must be
-        // an odd element before it
         for(int i = 1; i < n; i++) {
-
             if(nums1[i] % 2 == 1)
                 continue;
 
             if(smalloddpr[i-1] == false)
                 return false;
         }
-
         return true;
     }
 };
