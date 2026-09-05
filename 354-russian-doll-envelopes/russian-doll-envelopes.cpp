bcf098f4f478fdaf0c749ed7806692ec1b3c3253
Devansh Singh <devanshsingh75020@gmail.com>
2026-09-05 15:10:09 +0530

    Time: 50 ms (36.60%) | Memory: 81.7 MB (75.56%) - LeetSync

---
+++ 354-russian-doll-envelopes/russian-doll-envelopes.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    static bool cmp(vector<int>& a, vector<int>& b){
+        if(a[0] == b[0])
+            return a[1] > b[1];
+
+        return a[0] < b[0];
+    }
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+
+        sort(envelopes.begin(), envelopes.end(), cmp);
+
+        vector<int> lis;
+
+        for(int i = 0; i < envelopes.size(); i++) {
+
+            int h = envelopes[i][1];
+
+            // Binary search
+            int pos = lower_bound(lis.begin(), lis.end(), h) - lis.begin();
+
+            if(pos == lis.size())
+                lis.push_back(h);
+            else
+                lis[pos] = h;
+        }
+
+        return lis.size();
+    }
+};
