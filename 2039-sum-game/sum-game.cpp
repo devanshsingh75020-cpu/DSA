0f10c63de41bcebdd12f7d6dd3c1ff11a171ea9d
Devansh Singh <devanshsingh75020@gmail.com>
2026-09-18 16:15:58 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (14.08%) - LeetSync

--- 2039-sum-game/sum-game.cpp
+++ 2039-sum-game/sum-game.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
     bool sumGame(string num) {
-        int n=num.size();
-        int mid=n/2;
+        int n = num.size();
+        int mid = n / 2;
 
-        int leftsum=0,rightsum=0;
-        int leftcnt=0,rightcnt=0;
+        int leftsum = 0, rightsum = 0;
+        int leftcnt = 0, rightcnt = 0;
 
-        for(int i=0; i<mid; i++){
+        for(int i = 0; i < mid; i++){
             if(num[i] == '?'){
                 leftcnt++;
             }
             else{
-                leftsum+=num[i]-'0';
+                leftsum += num[i] - '0';
             }
         }
 
-        for(int i=mid; i<n; i++){
+        for(int i = mid; i < n; i++){
             if(num[i] == '?'){
                 rightcnt++;
             }
             else{
-                rightsum+=num[i]-'0';
+                rightsum += num[i] - '0';
             }
         }
 
-        if((leftcnt + rightcnt) % 2 != 0){
+        if((leftcnt + rightcnt) % 2 != 0)
             return true;
+
+         if(2 * (leftsum - rightsum) == 9 * (rightcnt - leftcnt)){
+            return false;
         }
-        
-        return 2*(leftsum - rightsum) != 9 *(rightcnt-leftcnt);
+
+        return true;
     }
 };
